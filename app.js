83aa52044c28b5de3a9e20d877de055ba0b26b61
Anshum <anshumdutta5@gmail.com>
2021-06-27 16:55:07 +0530

    Add Procfile

--- app.js
+++ app.js
@@ -385,6 +385,6 @@ app.post("/mail-features", function(req, res) {
   }
 })
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 })
